2b755eaed69a030617cb4bd3ec68d6e1f819e76d
Ben Radler <ben@boombotix.com>
2013-04-21 22:11:08 -0700

    fixing html, removing legacy css, adding resize bind

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,6 +1,11 @@
 var app = app || {};
 $(function() {
-
+  // start our backbone app
   window.dapp = new app.SwatchAppView();
   Backbone.history.start();
+
+  // bind to the jquery-ui-touch-punch resizable elements
+  $('ul#colors li').resizable({
+    // handles: 'e'
+  });
 });
